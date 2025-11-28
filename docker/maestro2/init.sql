81a94a37558b5cc780e73595d1b31bac5589fd06
Nino-FR-04 <davidfrx322@gmail.com>
2025-11-27 23:30:10 -0500

    Correcciones a los permisos

--- docker/maestro2/init.sql
+++ docker/maestro2/init.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE IF NOT EXISTS biblioteca;
 
-CREATE USER IF NOT EXISTS 'bib_repl'@'%' IDENTIFIED BY 'ucsm2025';
+CREATE USER IF NOT EXISTS 'bib_repl'@'%' IDENTIFIED WITH mysql_native_password BY 'ucsm2025';
 GRANT REPLICATION SLAVE ON *.* TO 'bib_repl'@'%';
 
 CREATE USER IF NOT EXISTS 'app_user'@'%' IDENTIFIED BY 'app2025';
