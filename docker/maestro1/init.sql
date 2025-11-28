c920c4c5a4e2ba896a29e5dc3a362f1da6286947
Nino-FR-04 <davidfrx322@gmail.com>
2025-11-27 22:23:25 -0500

    Correcciones generales a los archivos y cambios en la estructura del proyecto

--- docker/maestro1/init.sql
+++ docker/maestro1/init.sql
@@ -1,18 +1,19 @@
---Script - basico
 CREATE DATABASE IF NOT EXISTS biblioteca;
 
---Usuario y permisos
 CREATE USER IF NOT EXISTS 'bib_repl'@'%' IDENTIFIED BY 'ucsm2025';
 GRANT REPLICATION SLAVE ON *.* TO 'bib_repl'@'%';
+
+CREATE USER IF NOT EXISTS 'app_user'@'%' IDENTIFIED BY 'app2025';
+GRANT SELECT, INSERT, UPDATE, DELETE ON biblioteca.* TO 'app_user'@'%';
+
 FLUSH PRIVILEGES;
 
--- Use la base de datos biblioteca
 USE biblioteca;
--- Crear tabla libros
+
 CREATE TABLE IF NOT EXISTS libros (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    titulo VARCHAR(255) NOT NULL,
-    autor VARCHAR(255) NOT NULL,
+    titulo VARCHAR(255),
+    autor VARCHAR(255),
     anio_publicacion INT,
     genero VARCHAR(100)
 );
---
+++ docker/maestro2/init.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE IF NOT EXISTS biblioteca;
+
+CREATE USER IF NOT EXISTS 'bib_repl'@'%' IDENTIFIED BY 'ucsm2025';
+GRANT REPLICATION SLAVE ON *.* TO 'bib_repl'@'%';
+
+CREATE USER IF NOT EXISTS 'app_user'@'%' IDENTIFIED BY 'app2025';
+GRANT SELECT, INSERT, UPDATE, DELETE ON biblioteca.* TO 'app_user'@'%';
+
+FLUSH PRIVILEGES;
+
+USE biblioteca;
+
+CREATE TABLE IF NOT EXISTS libros (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    titulo VARCHAR(255),
+    autor VARCHAR(255),
+    anio_publicacion INT,
+    genero VARCHAR(100)
+);
